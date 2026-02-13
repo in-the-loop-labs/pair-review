731e2c4305ae72758da5fc930bc210254a18c9be
Aqeel Nazeer <aqeel.nazeer@shopify.com>
2026-02-13 14:38:42 +0530

    Add chat to file based comments

--- public/js/modules/file-comment-manager.js
+++ public/js/modules/file-comment-manager.js
@@ -369,6 +369,9 @@ class FileCommentManager {
               <path fill-rule="evenodd" d="M11.013 1.427a1.75 1.75 0 012.474 0l1.086 1.086a1.75 1.75 0 010 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 01-.927-.928l.929-3.25a1.75 1.75 0 01.445-.758l8.61-8.61zm1.414 1.06a.25.25 0 00-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 000-.354l-1.086-1.086zM11.189 6.25L9.75 4.81l-6.286 6.287a.25.25 0 00-.064.108l-.558 1.953 1.953-.558a.249.249 0 00.108-.064l6.286-6.286z"></path>
             </svg>
           </button>
+          <button class="btn-chat-comment comment-action-btn chat-btn" title="Chat about this comment">
+            ${CommentManager.CHAT_ICON_SVG}
+          </button>
           <button class="btn-delete-comment" title="Dismiss comment">
             <svg class="octicon" viewBox="0 0 16 16" width="16" height="16">
               <path fill-rule="evenodd" d="M6.5 1.75a.25.25 0 01.25-.25h2.5a.25.25 0 01.25.25V3h-3V1.75zm4.5 0V3h2.25a.75.75 0 010 1.5H2.75a.75.75 0 010-1.5H5V1.75C5 .784 5.784 0 6.75 0h2.5C10.216 0 11 .784 11 1.75zM4.496 6.675a.75.75 0 10-1.492.15l.66 6.6A1.75 1.75 0 005.405 15h5.19c.9 0 1.652-.681 1.741-1.576l.66-6.6a.75.75 0 00-1.492-.149l-.66 6.6a.25.25 0 01-.249.225h-5.19a.25.25 0 01-.249-.225l-.66-6.6z"></path>
@@ -379,11 +382,17 @@ class FileCommentManager {
       <div class="user-comment-body" data-original-markdown="${window.escapeHtmlAttribute(comment.body)}">${renderedBody}</div>
     `;
 
-    // Wire up edit/delete buttons
+    // Wire up edit/delete/chat buttons
     const editBtn = card.querySelector('.btn-edit-comment');
+    const chatBtn = card.querySelector('.btn-chat-comment');
     const deleteBtn = card.querySelector('.btn-delete-comment');
 
     editBtn.addEventListener('click', () => this.editFileComment(zone, comment));
+    if (chatBtn) {
+      chatBtn.addEventListener('click', () => {
+        this.prManager?.chatPanelManager?.openChat(comment.id);
+      });
+    }
     deleteBtn.addEventListener('click', () => this.deleteFileComment(zone, comment.id));
 
     // Insert before form if present, otherwise append
@@ -495,6 +504,10 @@ class FileCommentManager {
           <svg viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M11.013 1.427a1.75 1.75 0 012.474 0l1.086 1.086a1.75 1.75 0 010 2.474l-8.61 8.61c-.21.21-.47.364-.756.445l-3.251.93a.75.75 0 01-.927-.928l.929-3.25a1.75 1.75 0 01.445-.758l8.61-8.61zm1.414 1.06a.25.25 0 00-.354 0L10.811 3.75l1.439 1.44 1.263-1.263a.25.25 0 000-.354l-1.086-1.086zM11.189 6.25L9.75 4.81l-6.286 6.287a.25.25 0 00-.064.108l-.558 1.953 1.953-.558a.249.249 0 00.108-.064l6.286-6.286z"></path></svg>
           Edit
         </button>
+        <button class="ai-action ai-action-chat">
+          ${CommentManager.CHAT_ICON_SVG}
+          Chat
+        </button>
         <button class="ai-action ai-action-dismiss">
           <svg viewBox="0 0 16 16" width="16" height="16"><path fill-rule="evenodd" d="M3.72 3.72a.75.75 0 011.06 0L8 6.94l3.22-3.22a.75.75 0 111.06 1.06L9.06 8l3.22 3.22a.75.75 0 11-1.06 1.06L8 9.06l-3.22 3.22a.75.75 0 01-1.06-1.06L6.94 8 3.72 4.78a.75.75 0 010-1.06z"></path></svg>
           Dismiss
@@ -506,11 +519,17 @@ class FileCommentManager {
     const adoptBtn = card.querySelector('.ai-action-adopt');
     const dismissBtn = card.querySelector('.ai-action-dismiss');
     const editBtn = card.querySelector('.ai-action-edit');
+    const chatBtn = card.querySelector('.ai-action-chat');
     const restoreBtn = card.querySelector('.btn-restore');
 
     adoptBtn.addEventListener('click', () => this.adoptAISuggestion(zone, suggestion));
     dismissBtn.addEventListener('click', () => this.dismissAISuggestion(zone, suggestion.id));
     editBtn.addEventListener('click', () => this.editAndAdoptAISuggestion(zone, suggestion));
+    if (chatBtn) {
+      chatBtn.addEventListener('click', () => {
+        this.prManager?.chatPanelManager?.openChat(suggestion.id);
+      });
+    }
     restoreBtn.addEventListener('click', async () => await this.restoreAISuggestion(zone, suggestion.id));
 
     // Insert at the beginning (AI suggestions shown first)
